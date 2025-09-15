401d08509664c96cfae5cc16eece365c53c0efd1
TrosBriset7530 <Trosbriset@gmail.com>
2025-09-15 15:57:27 +0700

    noice

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+import datetime
+#1
 aList = ["John", 33, "Toronto", True]
 print(aList)
+#2
+x = datetime.date.today()
+date = x.strftime("%d-%m-%Y")
+mahasiswa = ["Rafael Octo", 2024071003, "Informatika", "DAA", date, "Universitas Pembangunan Jaya"]
+for i in mahasiswa:
+    print(i)
